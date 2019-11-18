ab7a9d560eaad4b3c0bfd6131b21dec28d4607fd
Emily Cain <ejcain@arizona.edu>
2019-11-18 12:39:46 -0700

    populated empty trait columns

--- src/generate_phenotype_table.py
+++ src/generate_phenotype_table.py
@@ -33,8 +33,6 @@ df_1 = df_0.drop(labels=['Unnamed: 0', 'checked', 'citation_id', 'city', 'scient
                         'species_id', 'author', 'citation_year', 'trait_description', 'units', 'n', 'statname',
                         'stat', 'access_level', 'view_url', 'edit_url'], axis=1)
 
-df_1.head()
-
 
 # #### Extract Range and Column Values
 
@@ -55,38 +53,113 @@ df_2['column'] = df_2['sitename'].str.extract("Column (\d+)").astype(int)
 # * Each trait should have its own column
 # * Rename `mean` column to `value` for easier understanding
 
-# In[10]:
+# In[7]:
 
 
 df_3 = df_2.rename({'mean': 'value'}, axis=1)
 
 
-# In[13]:
+# In[8]:
 
 
 traits_to_keep = ['leaf_temperature', 'ambient_humidity', 'proximal_air_temperature', 'surface_temperature',
                   'aboveground_dry_biomass', 'canopy_height', 'flag_leaf_emergence_time', 'flowering_time',
                   'canopy_cover']
 
+
+# In[9]:
+
+
 empty_df = pd.DataFrame(data=df_3, index=df_3.index, columns=traits_to_keep)
 
 
-# In[18]:
+# In[10]:
 
 
 df_4 = pd.concat([df_3, empty_df.reindex(df_3.index)], axis=1)
 
 
-# In[21]:
+# #### Drop more unecessary (at this time) columns
+
+# In[11]:
 
 
-timestamp = datetime.datetime.now().replace(microsecond=0).isoformat()
-output_filename = f'pheno-table_{timestamp}.csv'.replace(':', '')
-df_4.to_csv(f'../data/processed/{output_filename}', index=False)
+df_5 = df_4.drop(labels=['result_type', 'treatment_id', 'treatment', 'dateloc'], axis=1)
 
+
+# #### Populate empty columns with available values
+
+# In[12]:
+
+
+# This is very slow - needs refactoring for .py script and reproducible notebook 
+
+# run_slow_stuff = False
+
+# if run_slow_stuff:
+
+counter = 0
+
+for index, row in df_5.iterrows():
+    if counter % 1000 == 0:
+        print(counter)            
+        counter += 1
+    for trait in traits_to_keep:
+        if row['trait'] == trait:                
+            df_5.loc[index, [trait]] = row['value']
+
+
+# In[13]:
+
+
+df_6 = df_5.set_index('sitename')
+
+
+# #### Drop some columns that are redundant or can be explained in data dictionary
+# * `month`
+# * `year`
+# * `notes`
+# * `trait` - now have trait values in wide format (one column per trait requested for this iteration of dataset)
+# * `method_name`
+# * `notes` 
+
+# In[14]:
+
+
+df_6.drop(labels=['month', 'year', 'notes', 'trait', 'method_name', 'notes'], axis=1, inplace=True)
+
+
+# #### Read in `df_6` 
+
+# In[15]:
+
+
+# df_6 = pd.read_csv(('../data/processed/pheno-table_populated_traits_2019-11-18T071126.csv')
+
+
+# #### Add growing degree days
+
+# #### Add max canopy height
+
+# #### Re-order column names
+
+# #### Change plots to index
+
+# #### Add planting date column
 
 # In[ ]:
 
 
 
+
+
+# In[16]:
+
+
+# Update df_* 
+# df_6 will be renamed
+
+timestamp = datetime.datetime.now().replace(microsecond=0).isoformat()
+output_filename = f'pheno-table_{timestamp}.csv'.replace(':', '')
+df_6.to_csv(f'../data/processed/{output_filename}')
 
